daa51f7575a8309e3da491460c11b9c56f0e26c0
GerardRosario <sjvideokid590@gmail.com>
2023-09-28 17:14:39 -0700

    Cleaning App.js

--- movies-gold-v1/src/App.js
+++ movies-gold-v1/src/App.js
@@ -6,11 +6,6 @@ import {useState, useEffect} from 'react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-      </header>
     </div>
   );
 }
